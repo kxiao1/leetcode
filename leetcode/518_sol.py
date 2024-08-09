d4ec4039d0cfd2de890af7b0fcf88ecf5908c711
Karl Xiao <xzetianx1997@gmail.com>
2024-08-09 16:17:04 -0400

    Fix old solutions

--- leetcode/518_sol.py
+++ leetcode/518_sol.py
@@ -2,7 +2,7 @@
 # don't bother with intermediate counts
 # when x is some multiple k of coin, it automatically
 # adds to the combinations of using (k-1) of coin
-
+from typing import List
 def change(amount: int, coins: List[int]) -> int:
     dp = [0] * (amount + 1)
     dp[0] = 1
